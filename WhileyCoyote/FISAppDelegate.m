9fa21999db9a0d6103278ca8b80a03c17bc884b0
JohnPH75 <johnhussain75@gmail.com>
2016-05-13 11:27:24 -0400

    sixteenth commit

--- WhileyCoyote/FISAppDelegate.m
+++ WhileyCoyote/FISAppDelegate.m
@@ -11,9 +11,41 @@
     
      */
     
+    
+    NSUInteger steps = 0;
+    
+    NSUInteger anvil = arc4random_uniform(25) + 26;
+    
+    do {
+        
+        if (steps % 10 == 0 && steps > 0) { //checks whether integer "steps" is an even factor of 10 and also greater than 0
+            
+            NSLog(@"%lu YOU'RE CUCKOO!", steps);
+            
+        }
+        
+        else if (steps == anvil) {
+            
+            NSLog(@"%lu SMASH!", steps);
+        }
+        
+        else {
+        
+            NSLog(@"%lu Meep! Meep!", steps); //the @lu format specifier prints the value of NSInteger "steps"
+        }
+        
+        steps++;
+        
+    } while (steps <= anvil);
+    
+    
+    
     // do not alter
     return YES;  //
     ///////////////
 }
+
+
+
 
 @end
